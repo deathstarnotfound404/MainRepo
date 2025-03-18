fe1aaf72186f9df27f8c57ab6ab2b4ee287f4c16
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-18 17:12:44 +0100

    Initial commit
    Skeleton program vázának a létrehozása.

---
+++ skeleton/src/TestCases/Test31.java
@@ -0,0 +1,36 @@
+package TestCases;
+/**
+ * A {@code Test31} osztály a 31. szkeleton tesztesetet implementálja.
+ *
+ * <p>Az osztály tartalmazza a ... (attribútumok felsorolása).
+ * Lehetőséget biztosít az /num/. teszteset végrehajtására ami ... (mit csinál).</p>
+ *
+ * <p>Kapcsolódó osztályok:
+ * {@link Osztaly} - ... (funkcio: pl: EGy gombafonalhoz tartozó start és céltekton).</p>
+ *
+ * @author Your Name
+ * @version 1.0
+ * @since 2025-03-18
+ */
+
+public class Test31 implements ITestCase {
+    //Test attributes
+    /** Javadoc, attributumok leirasa. */
+
+
+    //Test init
+    /**
+     * Létrehoz egy új {@code Test1} objektumot.
+     */
+    public Test31() {
+
+    }
+
+    /**
+     * Végrehajtja a TestCase-hez tartozó tesztesetet.
+     */
+    @Override
+    public void runTest() {
+
+    }
+}
