39eebe3771e4efb8d08236bbbabb652381a98f8b
Máté Bekő <etamokeb@gmail.com>
2025-03-20 00:26:29 +0100

    Added FonalGatloHatas class

--- skeleton/src/FungoriumClasses/FonalGatloHatas.java
+++ skeleton/src/FungoriumClasses/FonalGatloHatas.java
@@ -1,4 +1,12 @@
 package FungoriumClasses;
 
 public class FonalGatloHatas extends TektonHatas{
+    public FonalGatloHatas() {
+        System.out.println("<<<return FonalGatloHatas()");
+    }
+
+    public String hatas() {
+        System.out.println("<<<return hatas()");
+        return "FonalGatloHatas";
+    }
 }
