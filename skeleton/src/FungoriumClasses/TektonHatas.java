439022fc78fa7d7b449d3816e56d479b66a5828b
Máté Bekő <etamokeb@gmail.com>
2025-03-20 00:22:56 +0100

    Added TektonHatas class

--- skeleton/src/FungoriumClasses/TektonHatas.java
+++ skeleton/src/FungoriumClasses/TektonHatas.java
@@ -1,4 +1,22 @@
 package FungoriumClasses;
 
 public class TektonHatas {
+
+    public TektonHatas() {
+        System.out.println("<<<return TektonHatas()");
+    }
+
+    public String hatas() {
+        System.out.println("<<<return hatas()");
+        return null;
+    }
+
+    public void setTekton(Tekton t) {
+        System.out.println("<<<return setTekton()");
+    }
+
+    public Tekton getTekton() {
+        System.out.println("<<<return getTekton()");
+        return null;
+    }
 }
