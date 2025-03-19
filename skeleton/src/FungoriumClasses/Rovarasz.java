2b68b111e4074411c4bc8c1c5c420b5bb2f36281
Botos Dániel <botosdaniel2003@gmail.com>
2025-03-20 00:30:58 +0100

    rovarasz class done

--- skeleton/src/FungoriumClasses/Rovarasz.java
+++ skeleton/src/FungoriumClasses/Rovarasz.java
@@ -1,5 +1,31 @@
 package FungoriumClasses;
 
 public class Rovarasz {
+    public Rovarasz() {
+        System.out.println("<<<return Rovarasz()");
+    }
 
+    public Rovar getRovar() {
+        System.out.println("<<<return getRovar()");
+        return new Rovar();
+    }
+
+    public void addRovar(Rovar r, Tekton t) {
+        System.out.println("<<<return addRovar()");
+    }
+
+    public int calcAllTapanyagScore() {
+        System.out.println("<<<return calcAllTapanyagScore()");
+        return 0;
+    }
+
+    public boolean rovarIranyitas(Rovar r, Tekton celTekton) {
+        System.out.println("<<<return rovarIranyitas()");
+        return false;
+    }
+
+    public boolean fonalVagas(Rovar r, Gombafonal gf) {
+        System.out.println("<<<return fonalVagas()");
+        return false;
+    }
 }
