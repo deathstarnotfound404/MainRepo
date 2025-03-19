7353aeb11864a6558d0e149a64688111a40a8834
Máté Bekő <etamokeb@gmail.com>
2025-03-20 00:27:41 +0100

    Added GombaTestGatloHatas class

--- skeleton/src/FungoriumClasses/GombaTestGatloHatas.java
+++ skeleton/src/FungoriumClasses/GombaTestGatloHatas.java
@@ -1,4 +1,12 @@
 package FungoriumClasses;
 
 public class GombaTestGatloHatas extends TektonHatas {
+    public GombaTestGatloHatas() {
+        System.out.println("<<<return GombaTestGatloHatas()");
+    }
+
+    public String hatas() {
+        System.out.println("<<<return hatas()");
+        return "GombaTestGatloHatas";
+    }
 }
