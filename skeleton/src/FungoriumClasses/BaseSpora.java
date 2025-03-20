0b249a14fc2bd497bb9fec9ac765bbef1b5056f4
Pünkösti Györk <punkostigyorke@gmail.com>
2025-03-20 16:55:16 +0200

    Add skeleton-gyork branch with code

--- skeleton/src/FungoriumClasses/BaseSpora.java
+++ skeleton/src/FungoriumClasses/BaseSpora.java
@@ -1,8 +1,30 @@
 package FungoriumClasses;
 
 public abstract class BaseSpora implements IDestroyable {
+    //az alapertelmezett erteke annak, hogy egy spora mennyi tapanyagot tartalmaz
+    protected int tapanyag = 1;
+
+    //Konstructor
+    public BaseSpora() {
+        System.out.println("<<<return BaseSpora()");
+    }
+
+    //Absztrakt metodus, amit az alos
+    public abstract void hatas(Rovar r);
+
+    public int getTapanyag() {
+        System.out.println("<<<return getTapanyag()");
+        return tapanyag;
+    }
+
+    //A tapanyag erteket beallito setter metodus
+    public void setTapanyag(int t) {
+        tapanyag = t;
+        System.out.println("<<<return setTapanyag()");
+    }
+
     @Override
     public void elpusztul() {
-
+        System.out.println("<<<retrun elpusztul()");
     }
 }
--- skeleton/src/FungoriumClasses/Spora.java
+++ skeleton/src/FungoriumClasses/Spora.java
@@ -1,4 +1,14 @@
 package FungoriumClasses;
 
 public class Spora extends BaseSpora {
+    //Spora konstruktora
+    public Spora() {
+        super();
+        System.out.println("<<<return Spora()");
+    }
+
+    @Override
+    public void hatas(Rovar r) {
+        System.out.println("<<<return hatas()");
+    }
 }
