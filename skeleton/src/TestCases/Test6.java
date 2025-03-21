2db93ecb334e992531c65a130b6d8428d05905c3
KozmaSzabolcsAndras <koszabi7@gmail.com>
2025-03-21 13:16:26 +0100

    test6

--- skeleton/src/TestCases/Test6.java
+++ skeleton/src/TestCases/Test6.java
@@ -1,6 +1,8 @@
 package TestCases;
 
 import CallTracer.CallTracer;
+import FungoriumClasses.*;
+import java.util.*;
 
 /**
  * A {@code Test6} osztály a hatodik szkeleton tesztesetet implementálja.
@@ -20,6 +22,11 @@ public class Test6 extends TestCase implements ITestCase {
     //Test attributes
     /** Javadoc, attributumok leirasa. */
 
+    Gombasz gsz;
+    Gomba g;
+    GombaTest gt;
+    TektonHatas th2;
+    Tekton t2;
 
     //Test init
     /**
@@ -27,6 +34,11 @@ public class Test6 extends TestCase implements ITestCase {
      */
     public Test6(CallTracer callTracer) {
         super(callTracer);
+        gsz = new Gombasz();
+        th2 = new TektonHatas();
+        t2 = new Tekton(th2);
+        g = new Gomba(t2);
+        gt = new GombaTest(g, 3);
     }
 
     /**
@@ -34,6 +46,31 @@ public class Test6 extends TestCase implements ITestCase {
      */
     @Override
     public void runTest() {
+        CallTracer.enter("getSzorasCount", "GombaTest", "");
+        int szorasCount = gt.getSzorasCount();
+        CallTracer.exit("getSzorasCount", "szorasCount");
+        int choice = makeDecision("Hármas szinten van-e a gomba?", Arrays.asList("Nem", "Igen"));
+        switch (choice) {
+            case 1: //if(szorasCount % 3 == 0 && szorasCount != 9)
+                CallTracer.enter("szintlepes", "GombaTest", "szorasCount");
+                gt.szintlepes(szorasCount);
+                CallTracer.exit("szintlepes", "");
+                break;
 
+            case 2://if(szorasCount == 9)
+                CallTracer.enter("szintlepes", "GombaTest", "szorasCount");
+                gt.szintlepes(szorasCount);
+                CallTracer.exit("szintlepes", "");
+                CallTracer.enter("elpusztul", "Gomba", "");
+                g.elpusztul();
+                CallTracer.exit("elpusztul", "");
+                CallTracer.enter("elpusztul", "GombaTest", "");
+                gt.elpusztul();
+                CallTracer.exit("elpusztul", "");
+                break;
+
+            default:
+                break;
+        }
     }
 }
