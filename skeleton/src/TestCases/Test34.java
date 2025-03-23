80f14f17a6f74db546e1f8c439ef0e19bbedd664
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-23 19:44:00 +0100

    Test34 dokumentációja

--- skeleton/src/TestCases/Test34.java
+++ skeleton/src/TestCases/Test34.java
@@ -5,17 +5,35 @@ import CallTracer.CallTracer;
 /**
  * A {@code Test34} osztály a 34. szkeleton tesztesetet implementálja.
  *
- * <p>Az osztály tartalmazza a ... (attribútumok felsorolása).
- * Lehetőséget biztosít az /num/. teszteset végrehajtására ami ... (mit csinál).</p>
+ * <p><b>34: Gombafonal Irányítás Teszt - Fonal nem lerakható</b></p>
  *
- * <p>Kapcsolódó osztályok:
- * {@link Osztaly} - ... (funkcio: pl: EGy gombafonalhoz tartozó start és céltekton).</p>
+ * <p><b>Rövid leírás:</b><br>
+ * A teszteset a gombász fonal lerakásának azon esetét vizsgálja, amikor nem tud lerakni.</p>
+ *
+ * <p><b>Aktorok:</b><br>
+ * Tesztelő, Skeleton</p>
+ *
+ * <p><b>Forgatókönyv:</b></p>
+ * <ol>
+ *     <li>Az ellenőrzés során vagy nincs elég fonal a Gombász fonal készletében, vagy a megadott start és cél tektonok nem szomszédosak.</li>
+ *     <li>Döntés: Nincs elég fonal vagy nem szomszédosak a Tektonok:</li>
+ *         <ol>
+ *             <li>Nincs elég fonal</li>
+ *             <li>Nem szomszédos Tektonok</li>
+ *         </ol>
+ *     <li>Ha egyik feltétel sem teljesül, akkor a művelet nem hajtódik végre.</li>
+ * </ol>
+ *
+ * <p><b>Kapcsolódó osztályok:</b></p>
+ * <ul>
+ *     <li>{@link Gombasz} - A tesztelt objektum, amely a gombafonal irányítását végzi.</li>
+ *     <li>{@link Tekton} - A tesztben szereplő környezeti elem.</li>
+ * </ul>
  *
  * @author Your Name
  * @version 1.0
  * @since 2025-03-18
  */
-
 public class Test34 extends TestCase implements ITestCase {
     //Test attributes
     /** Javadoc, attributumok leirasa. */
