735f88658f48239a176f0080a7bed013f3198019
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-23 20:41:07 +0100

    BaseSpora dokumentációja

--- skeleton/src/FungoriumClasses/BaseSpora.java
+++ skeleton/src/FungoriumClasses/BaseSpora.java
@@ -1,27 +1,59 @@
 package FungoriumClasses;
 
+/**
+ * A {@code BaseSpora} absztrakt osztály a spóra objektumok alapvető tulajdonságait és működését határozza meg.
+ *
+ * <p>Az osztály egy spóra alapvető tápanyagértékét tárolja, valamint tartalmazza a spórák hatásának és elpusztulásának kezelését.</p>
+ *
+ * <p>Az osztály az {@link IDestroyable} interfészt valósítja meg, így minden spóra elpusztítható.</p>
+ *
+ * @author NAME
+ * @version 1.0
+ * @since 2025-03-19
+ */
+
 public abstract class BaseSpora implements IDestroyable {
-    //az alapertelmezett erteke annak, hogy egy spora mennyi tapanyagot tartalmaz
+    /** Az alapértelmezett tápanyagérték, amelyet egy spóra tartalmaz. */
     protected int tapanyag = 1;
 
-    //Konstructor
+    /**
+     * Létrehoz egy új {@code BaseSpora} objektumot alapértelmezett beállításokkal.
+     */
     public BaseSpora() {
     }
 
-    //Absztrakt metodus, amit az alosztalyok fognak implementalni a hatas miatt
+    /**
+     * Meghatározza a spóra hatását egy adott rovarra.
+     *
+     * <p>Ezt a metódust az alosztályok implementálják a konkrét hatások meghatározására.</p>
+     *
+     * @param r A rovar, amelyre a spóra hatással lesz.
+     */
     public abstract void hatas(Rovar r);
 
-    //tapanyag erteket visszaterito getter
+    /**
+     * Visszaadja a spóra aktuális tápanyagértékét.
+     *
+     * @return A spóra tápanyagértéke.
+     */
     public int getTapanyag() {
         return tapanyag;
     }
 
-    //A tapanyag erteket beallito setter metodus
+    /**
+     * Beállítja a spóra tápanyagértékét.
+     *
+     * @param t Az új tápanyagérték.
+     */
     public void setTapanyag(int t) {
         tapanyag = t;
     }
 
+    /**
+     * A spóra elpusztításának metódusa.
+     *
+     * <p>Ez az implementáció jelenleg üres, de az alosztályok felülírhatják a megfelelő működés érdekében.</p>
+     */
     @Override
-    public void elpusztul() {
-    }
+    public void elpusztul() {}
 }
