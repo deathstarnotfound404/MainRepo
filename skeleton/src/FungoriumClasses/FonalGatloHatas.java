cc67d124cedc8441868e3a12b4cd414be00d5cb3
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-23 20:56:47 +0100

    FonalGAtloHatas dokumentálása

--- skeleton/src/FungoriumClasses/FonalGatloHatas.java
+++ skeleton/src/FungoriumClasses/FonalGatloHatas.java
@@ -1,11 +1,40 @@
 package FungoriumClasses;
+import CallTracer.*;
 
-import CallTracer.CallTracer;
-
+/**
+ * A {@code FonalGatloHatas} osztály egy speciális {@link TektonHatas}, amely meggátolja a további fonalak növesztését az adott Tektonon.
+ *
+ * <p>Amikor a hatás aktiválódik, a tekton {@code fokszam} értékét lekéri,
+ * és ennek alapján eldönti, hogy van-e gátló fonal a tektonon.</p>
+ *
+ * <p>Kapcsolódó osztályok:</p>
+ * <ul>
+ *     <li>{@link TektonHatas} - Az osztály, amelyből ez az osztály származik.</li>
+ *     <li>{@link Tekton} - Az osztály, amelyen a hatás végrehajtódik.</li>
+ *     <li>{@link CallTracer} - A híváskövetési mechanizmust biztosító osztály.</li>
+ * </ul>
+ *
+ * @author NAME
+ * @version 1.0
+ * @since 2025-03-18
+ */
 public class FonalGatloHatas extends TektonHatas{
+
+    /**
+     * Létrehoz egy új {@code FonalGatloHatas} objektumot.
+     */
     public FonalGatloHatas() {
+        super();
     }
 
+    /**
+     * Kifejti a hatást az adott tektonon.
+     *
+     * <p>A metódus először lekéri a hatáshoz tartozó {@link Tekton} objektumot,
+     * majd meghívja rajta a {@code getFokszam()} metódust.</p>
+     *
+     * @return A hatás eredményétől függően "FonalGatlo" vagy "NincsFonalGatlo" szöveget ad vissza, amit majd a kontroller tudtára kell adni.
+     */
     public String hatas() {
         Tekton t1 = this.getTekton();
         CallTracer.enter("getFokszam", "Tekton", "");
