d0c4614711da20e59564730c5afae0d3ff141af3
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-23 21:49:20 +0100

    TektonHatas dok

--- skeleton/src/FungoriumClasses/TektonHatas.java
+++ skeleton/src/FungoriumClasses/TektonHatas.java
@@ -1,21 +1,44 @@
 package FungoriumClasses;
 
+/**
+ * A TektonHatas osztály egy tektonhoz társított hatást reprezentál.
+ * Az osztály lehetőséget biztosít egy tekton tárolására és egy alapértelmezett hatás visszaadására.
+ */
 public class TektonHatas {
 
     private Tekton tekton;
 
+    /**
+     * Konstruktor, amely létrehoz egy TektonHatas példányt.
+     */
     public TektonHatas() {
 
     }
 
+    /**
+     * Visszaadja a tekton hatását.
+     * Az alapértelmezett implementáció "Base" értéket ad vissza.
+     *
+     * @return A tekton hatását reprezentáló szöveg.
+     */
     public String hatas() {
         return "Base";  //Alapértelmezett működés
     }
 
+    /**
+     * Beállítja a hozzá tartozó tekton objektumot.
+     *
+     * @param t A beállítandó tekton.
+     */
     public void setTekton(Tekton t) {
         this.tekton = t;
     }
 
+    /**
+     * Visszaadja a társított tekton objektumot.
+     *
+     * @return A társított tekton, vagy {@code null}, ha nincs beállítva.
+     */
     public Tekton getTekton() {
         return this.tekton;
     }
