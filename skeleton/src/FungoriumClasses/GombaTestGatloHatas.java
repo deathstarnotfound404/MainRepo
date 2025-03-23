c141bf94ddb6dd6fdb2cb39e02bf679482daa0fc
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-23 21:27:42 +0100

    GTGatloH dokumetálása

--- skeleton/src/FungoriumClasses/GombaTestGatloHatas.java
+++ skeleton/src/FungoriumClasses/GombaTestGatloHatas.java
@@ -1,11 +1,25 @@
 package FungoriumClasses;
-import CallTracer.CallTracer;
+import CallTracer.*;
 
+/**
+ * A {@code GombaTestGatloHatas} osztály egy olyan hatást reprezentál,
+ * amely gátolja a gombatest fejlődését egy adott {@code Tekton}-on.
+ */
 public class GombaTestGatloHatas extends TektonHatas {
+    /**
+     * Létrehoz egy új {@code GombaTestGatloHatas} objektumot.
+     */
     public GombaTestGatloHatas() {
         super();
     }
 
+    /**
+     * Alkalmazza a gátló hatást a kapcsolódó {@code Tekton} objektumra.
+     * A hatás során a {@code Tekton} állapota megváltozik úgy,
+     * hogy jelezze: gombatest jelen van rajta.
+     *
+     * @return A hatás neve: {@code "GombaTestGatlo"}.
+     */
     public String hatas() {
         Tekton t1 = this.getTekton();
         CallTracer.enter("setVanGombaTest", "Tekton", "true");
