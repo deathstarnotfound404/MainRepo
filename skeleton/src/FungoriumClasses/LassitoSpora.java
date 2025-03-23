27149975372a7c7a8ff3add2391dee5deac62841
Czene Zsombor <czene.zsombor@icloud.com>
2025-03-23 21:31:35 +0100

    LassitoSp dokumentálása

--- skeleton/src/FungoriumClasses/LassitoSpora.java
+++ skeleton/src/FungoriumClasses/LassitoSpora.java
@@ -1,14 +1,23 @@
 package FungoriumClasses;
+import CallTracer.*;
 
-
-import CallTracer.CallTracer;
-
+/**
+ * A {@code LassitoSpora} osztály a lassító hatású spórát reprezentálja, amely csökkenti a rovarok fogyasztási képességét.
+ */
 public class LassitoSpora extends BaseSpora {
+    /**
+     * Létrehoz egy új {@code LassitoSpora} objektumot.
+     */
     public LassitoSpora() {
         super();
     }
 
-    //hatas kifejtese
+    /**
+     * A spóra hatásának alkalmazása egy rovarra.
+     * A spóra csökkenti a rovar maximális fogyasztási képességét és evési hatékonyságát.
+     *
+     * @param r A rovar, amelyre a spóra hatást gyakorol.
+     */
     @Override
     public void hatas(Rovar r) {
         CallTracer.enter("setMaxFogyasztas", "Rovar", "false");
