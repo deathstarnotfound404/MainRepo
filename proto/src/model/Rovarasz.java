30478026c3a365ef85ae3f226c8599becaa43661
Pünkösti Györk <punkostigyorke@gmail.com>
2025-04-24 00:37:42 +0300

    Implement Gombasz.java and Rovarasz.java

--- proto/src/model/Rovarasz.java
+++ proto/src/model/Rovarasz.java
@@ -3,11 +3,42 @@ import java.util.*;
 
 public class Rovarasz extends Player {
     private List<Rovar> rovarLista;
-    public Rovarasz() {}
-    public void addRovar(Rovar r, Tekton t) {}
-    public int calcAllTapanyagScore() { return 0; }
-    public boolean fonalVagas(Rovar r, GombaFonal gf) { return false; }
-    public List<Rovar> getRovarLista() { return null; }
-    public void removeRovar(Rovar r) {}
-    public boolean rovarIranyitas(Rovar r, Tekton celTekton) { return false; }
+
+    public Rovarasz() {
+        rovarLista = new ArrayList<Rovar>();
+    }
+
+    public void addRovar(Rovar r, Tekton t) {
+        if(t.vanRovarATektonon()) {
+            throw new Exception("Van rovar a tektonon.");
+        }
+         t.setRovar(r);
+    }
+
+    public int calcAllTapanyagScore() {
+        int sumTapanyag = 0;
+        for(Rovar r : rovarLista) {
+            sumTapanyag += r.getTapanyag();
+        }
+        return sumTapanyag;
+    }
+
+    public boolean fonalVagas(Rovar r, GombaFonal gf) {
+        if (r.getTudVagni()) {
+            r.vag(gf);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<Rovar> getRovarLista() {
+        return rovarLista;
+    }
+    public void removeRovar(Rovar r) {
+        rovarLista.remove(r);
+    }
+    public boolean rovarIranyitas(Rovar r, Tekton celTekton) {
+        return r.lep(celTekton);
+    }
 }
