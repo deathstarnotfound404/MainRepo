c63dbaa5fb0272886aa2fa3db1086df2c74255c9
Botos Dániel <botosdaniel2003@gmail.com>
2025-04-27 17:04:56 +0200

    tell my why

--- proto/src/model/KlonozoSpora.java
+++ proto/src/model/KlonozoSpora.java
@@ -1,10 +1,28 @@
 package model;
 
+/**
+ * Represents a special type of spore that can cause an insect to clone itself.
+ * When consumed by an insect, this spore triggers the cloning process of the insect,
+ * potentially creating a duplicate insect in the game environment.
+ * Extends the BaseSpora class.
+ */
 public class KlonozoSpora extends BaseSpora {
+
+    /**
+     * Constructs a new KlonozoSpora with default properties.
+     * Inherits ID generation from the BaseSpora class.
+     */
     public KlonozoSpora() {
         super();
     }
 
+    /**
+     * Implements the effect this spore has when consumed by an insect.
+     * Triggers the insect's cloning mechanism, which may create a duplicate insect.
+     * The success of cloning depends on the implementation in the Rovar class.
+     *
+     * @param r the insect that consumes this spore
+     */
     @Override
     public void hatas(Rovar r) {
         //Ha null-t ad vissza akkor sikertelen, ha r rovart akkor sikeres
--- proto/src/model/LassitoSpora.java
+++ proto/src/model/LassitoSpora.java
@@ -1,10 +1,28 @@
 package model;
 
+/**
+ * Represents a special type of spore that negatively impacts an insect's ability to consume.
+ * When consumed by an insect, this spore reduces the insect's eating capabilities by
+ * setting its eating efficiency to a low value (0.25) and disabling maximum consumption capacity.
+ * This has the opposite effect of GyorsitoSpora.
+ */
 public class LassitoSpora extends BaseSpora {
+
+    /**
+     * Constructs a new LassitoSpora with default properties.
+     * Inherits ID generation from the BaseSpora class.
+     */
     public LassitoSpora() {
         super();
     }
 
+    /**
+     * Implements the effect this spore has when consumed by an insect.
+     * Reduces the insect's eating efficiency to 25% and disables
+     * its maximum consumption capability.
+     *
+     * @param r the insect that consumes this spore
+     */
     @Override
     public void hatas(Rovar r) {
         r.setEvesHatekonysag(0.25);
