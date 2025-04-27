30eaeb0bdbdda01cd1d10409df9b0a9fdd13dca0
Botos Dániel <botosdaniel2003@gmail.com>
2025-04-27 16:50:40 +0200

    tell my why

--- proto/src/model/BaseSpora.java
+++ proto/src/model/BaseSpora.java
@@ -1,18 +1,42 @@
 package model;
 import java.util.*;
 
+/**
+ * Abstract base class representing a spore in the game.
+ * Spores have different effects when consumed by insects (Rovar).
+ * Implements the IDestroyable interface for lifecycle management.
+ */
 public abstract class BaseSpora implements IDestroyable {
+    /** The nutritional value of the spore */
     protected int tapanyag = 1;
+
+    /** Unique identifier for the spore */
     protected int id;
+
+    /** Random number generator for spore creation */
     private static final Random rnd = new Random();
 
+    /**
+     * Constructs a new spore with a unique ID.
+     */
     protected BaseSpora() {
         // konstruktor
         id = Field.genID();
     }
 
+    /**
+     * Defines the effect this spore has when consumed by an insect.
+     * To be implemented by specific spore types.
+     *
+     * @param r the insect that consumes the spore
+     */
     public abstract void hatas(Rovar r);
 
+    /**
+     * Creates a random spore of one of the six possible types.
+     *
+     * @return a randomly generated spore
+     */
     public static BaseSpora generateRandomSpora(){
         int valasztas = rnd.nextInt(6);     //6 féle spóra létezik
 
@@ -40,14 +64,32 @@ public abstract class BaseSpora implements IDestroyable {
         }
     }
 
+    /**
+     * Gets the nutritional value of the spore.
+     *
+     * @return the nutritional value
+     */
     public int getTapanyag() {
         return tapanyag;
     }
+
+    /**
+     * Sets the nutritional value of the spore.
+     *
+     * @param val the new nutritional value
+     */
     public void setTapanyag(int val) { this.tapanyag = val; }
 
+    /**
+     * Gets the unique identifier of the spore.
+     *
+     * @return the spore's ID
+     */
     public int getId() { return id; }
 
-    public void elpusztul() {
-        //TODO
-    }
+    /**
+     * Handles the destruction of the spore.
+     * Implementation of the IDestroyable interface.
+     */
+    public void elpusztul() { }
 }
--- proto/src/model/FonalDefenderHatas.java
+++ proto/src/model/FonalDefenderHatas.java
@@ -1,10 +1,27 @@
 package model;
 
+/**
+ * Represents a special tekton effect that protects connected fungal threads from being cut.
+ * When activated, this effect enables thread defense for the tekton it's applied to.
+ * Extends the base TektonHatas class.
+ */
 public class FonalDefenderHatas extends TektonHatas {
+
+    /**
+     * Constructs a new FonalDefenderHatas with default properties.
+     * Inherits from the TektonHatas class.
+     */
     public FonalDefenderHatas() {
         super();
     }
 
+    /**
+     * Implements the effect of this tekton action.
+     * Activates protection for all threads connected to this tekton,
+     * preventing them from being cut by insects.
+     *
+     * @return a string identifier "DefenderHatas" representing this effect
+     */
     @Override
     public String hatas() {
         super.tekton.setDefendFonalak(true);
